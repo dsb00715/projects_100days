78ade939f86f299e7a950b9bd2e2ace14a8b82a7
Deep Sureshkumar Bhatt <UI920363@rwe-ag.com>
2022-12-16 18:25:55 +0100

    day 11 code finished!

--- day11/proj_day11.py
+++ day11/proj_day11.py
@@ -3,8 +3,11 @@ import random
 from os import system
 
 
-def add(user_list, comp_list):
-    pass
+def ace_calculation(l_list):
+    l_list = [1 if i == 11 else i for i in l_list]
+    while sum(l_list) < 18:
+        l_list.append(random.sample(list(set(cards)), 1)[0])
+    return l_list
 
 
 def calculate_winner(user_list, comp_list):
@@ -14,11 +17,21 @@ def calculate_winner(user_list, comp_list):
         print("Congratulations! You win with a Blackjack!")
     elif sum(comp_list) == 21:
         print("Oh no! Computer wins with a Blackjack!")
+    elif sum(user_list) > 21:
+        print("You loose! you went over!")
+    elif sum(comp_list) > 21:
+        print("You win! Computer went over!")
+    elif sum(user_list) > sum(comp_list):
+        print("Congratulations! You win!")
+    elif sum(comp_list) > sum(user_list):
+        print("Oh no! You loose. try again!")
+    elif sum(user_list) == sum(comp_list):
+        print("It's a draw!")
 
 
 print(logo)
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-# cards = [11, 10]
+
 main_loop = True
 while main_loop:
     cont = True
@@ -42,11 +55,51 @@ while main_loop:
             while should_continue:
                 play_again = input("Type 'y' to get another card, Type 'n' to pass:")
                 if play_again == "n":
-                    calculate_winner(user_list, comp_list)
-                    should_continue = False
-                    break
+                    if sum(comp_list) >= 18:
+                        if sum(comp_list) > 21 and (11 in comp_list):
+                            comp_list = ace_calculation(comp_list)
+                        calculate_winner(user_list, comp_list)
+                        should_continue = False
+                        cont = False
+                    else:
+                        while sum(comp_list) < 18:
+                            comp_list.append(random.sample(list(set(cards)), 1)[0])
+                        if sum(comp_list) > 21 and (11 in comp_list):
+                            comp_list = ace_calculation(comp_list)
+                        calculate_winner(user_list, comp_list)
+                        should_continue = False
+                        cont = False
+
                 elif play_again == "y":
-                    add(user_list, comp_list)
+                    if sum(comp_list) <= 18:
+                        user_list.append(random.sample(list(set(cards)), 1)[0])
+                        comp_list.append(random.sample(list(set(cards)), 1)[0])
+                        if sum(comp_list) > 21 and (11 in comp_list):
+                            comp_list = ace_calculation(comp_list)
+                        elif sum(user_list) > 21 and (11 in user_list):
+                            user_list = ace_calculation(user_list)
+                        print(
+                            f"your cards: {user_list}, current score: {sum(user_list)}"
+                        )
+                        print(f"Computer's first card:{comp_list[0]}")
+                        if sum(user_list) >= 21 or sum(comp_list) >= 21:
+                            calculate_winner(user_list=user_list, comp_list=comp_list)
+                            should_continue = False
+                            cont = False
+                    else:
+                        user_list.append(random.sample(list(set(cards)), 1)[0])
+                        if sum(comp_list) > 21 and (11 in comp_list):
+                            comp_list = ace_calculation(comp_list)
+                        elif sum(user_list) > 21 and (11 in user_list):
+                            user_list = ace_calculation(user_list)
+                        print(
+                            f"your cards: {user_list}, current score: {sum(user_list)}"
+                        )
+                        print(f"Computer's first card:{comp_list[0]}")
+                        if sum(user_list) >= 21 or sum(comp_list) >= 21:
+                            calculate_winner(user_list=user_list, comp_list=comp_list)
+                            should_continue = False
+                            cont = False
                 else:
                     print("please input either 'y' or 'n':")
         elif user_play == "n":
