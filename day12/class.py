ea51663677c301aec1cf7c5f0e4f3f1694d26c41
Deep Sureshkumar Bhatt <UI920363@rwe-ag.com>
2022-12-19 17:57:29 +0100

    Day 12 work finished, Added comments on day 11,

--- day12/class.py
+++ day12/class.py
@@ -1,10 +1,35 @@
 ################### Scope ####################
 
+# enemies = 1
+
+
+# def increase_enemies():
+#     enemies = 2
+#     print(f"enemies inside function: {enemies}")
+
+
+# increase_enemies()
+# print(f"enemies outside function: {enemies}")
+
+
+""" enemies = "xy"
+
+
+def increase_enemies():
+    enemies = "ab"
+    print(f"enemies inside function: {enemies}")
+
+
+increase_enemies()
+print(f"enemies outside function: {enemies}") """
+
+
 enemies = 1
 
 
 def increase_enemies():
-    enemies = 2
+    global enemies
+    enemies += 1
     print(f"enemies inside function: {enemies}")
 
 
