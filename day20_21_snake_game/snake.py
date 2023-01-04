8fc7b7e38ea7747e455773072d55a76098862bae
Deep Sureshkumar Bhatt <UI920363@rwe-ag.com>
2023-01-04 16:42:00 +0100

    Snake game 3 TODOs completed!

--- day20_21_snake_game/snake.py
+++ day20_21_snake_game/snake.py
@@ -1,26 +1,55 @@
 from turtle import Turtle
 
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 
 class Snake:
-    """class to generate 3 snake instances & create basic snake body"""
+    """class to generate 3 snake instances & create basic snake body."""
 
     def __init__(self):
-        """_summary_"""
+        """When called, this will display basic Snake body."""
         self.snake_body = []
-        self.x_cor = 0
+        self.snake()
+        self.snake_head = self.snake_body[0]
 
+    def snake(self):
+        x_cor = 0
         for _ in range(3):
             self.snake = Turtle(shape="square")
             self.snake.color("white")
             self.snake.penup()
-            self.snake.goto(x=self.x_cor, y=0)
+            self.snake.goto(x=x_cor, y=0)
             self.snake_body.append(self.snake)
-            self.x_cor -= 20
+            x_cor -= 20
 
     def move(self):
-        """when called, this function will keep moving snake in forward direction"""
+        """when called, Snake will start moving in forward direction."""
         for index in range(len(self.snake_body) - 1, 0, -1):
             new_x = self.snake_body[index - 1].xcor()
             new_y = self.snake_body[index - 1].ycor()
             self.snake_body[index].goto(x=new_x, y=new_y)
-        self.snake_body[0].forward(20)
+        self.snake_head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        """when called, Snake will turn 90°."""
+        if self.snake_head.heading() != DOWN:
+            self.snake_head.setheading(UP)
+
+    def down(self):
+        """when called, Snake will turn 270°."""
+        if self.snake_head.heading() != UP:
+            self.snake_head.setheading(DOWN)
+
+    def left(self):
+        """when called, Snake will turn 180°."""
+        if self.snake_head.heading() != RIGHT:
+            self.snake_head.setheading(LEFT)
+
+    def right(self):
+        """when called, Snake will turn 0°."""
+        if self.snake_head.heading() != LEFT:
+            self.snake_head.setheading(RIGHT)
