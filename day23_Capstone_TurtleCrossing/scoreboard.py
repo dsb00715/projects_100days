a8e4da9673484b66dc09d1e8a308cb03de0f1818
Deep Sureshkumar Bhatt <UI920363@rwe-ag.com>
2023-01-10 11:46:24 +0100

    Day23 capstone project finished!

--- day23_Capstone_TurtleCrossing/scoreboard.py
+++ day23_Capstone_TurtleCrossing/scoreboard.py
@@ -1,5 +1,30 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        """Creates scoreboard"""
+        self.level = 1
+        self.color("black")
+        self.hideturtle()
+        self.penup()
+        self.goto(-280, 250)
+        self.update_level()
+
+    def update_level(self):
+        """displays scoreboard"""
+        self.write(f"LEVEL:{self.level}", font=FONT)
+
+    def increase_level(self):
+        """Increases level on the scoreboard"""
+        self.level += 1
+        self.clear()
+        self.update_level()
+
+    def game_over(self):
+        """Game over sequence"""
+        self.goto(-120, 0)
+        self.write(f"GAME OVER!", font=FONT)
