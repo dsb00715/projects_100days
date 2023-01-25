0ebba0d2f94314708013793c68948ab411430ad6
Deep Sureshkumar Bhatt <UI920363@rwe-ag.com>
2023-01-25 22:18:27 +0100

    Day-30 - Completed Password Manager

--- day26_NATOAlphabet/nato_alphabet/main.py
+++ day26_NATOAlphabet/nato_alphabet/main.py
@@ -8,7 +8,13 @@ nato_dict = {row["letter"]: row["code"] for i, row in data.iterrows()}
 
 
 # [x] TODO-2. Create a list of the phonetic code words from a word that the user inputs.
-
-user_word = input("Enter a word: ").upper()
-nato_list = [nato_dict[letter] for letter in user_word]
-print(nato_list)
+should_continue = True
+while should_continue:
+    try:
+        user_word = input("Enter a word: ").upper()
+        nato_list = [nato_dict[letter] for letter in user_word]
+    except KeyError:
+        print("Sorry only letters in the alphabet please.")
+    else:
+        print(nato_list)
+        should_continue = False
--- day29_Tkinter_PasswordGenerator/main.py
+++ day29_Tkinter_PasswordGenerator/main.py
@@ -3,11 +3,42 @@ from tkinter import *
 from tkinter import messagebox
 from password_generator import create_password
 from pyperclip import copy
+import json
 
 # ---------------------------- CONSTANTS ------------------------------- #
 FONT_NAME = "Courier"
 LOGO_PATH = "./day29_Tkinter_PasswordGenerator/logo.png"
-FILE_PATH = "./day29_Tkinter_PasswordGenerator/data.txt"
+FILE_PATH = "./day29_Tkinter_PasswordGenerator/data.json"
+
+# ---------------------------- Search functionality ------------------------------- #
+def search():
+    website_name = website_entry.get().lower()
+    try:
+        with open(FILE_PATH, "r") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        messagebox.showwarning(title="Error", message="No data File found!")
+    else:
+        if len(website_name) != 0:
+            if website_name in data:
+                email = str(data[website_name]["email"])
+                password = data[website_name]["password"]
+                note = data[website_name]["note"]
+                messagebox.showinfo(
+                    title=f"{website_name}",
+                    message=f"Email: {email}\nPassword: {password}\nNote: {note}\n\nPassword is copied to your clipboard.\nPress ok to paste",
+                )
+                copy(password)
+            else:
+                messagebox.showwarning(
+                    title="Error",
+                    message=f"No details for {website_name} exists.",
+                )
+        else:
+            messagebox.showwarning(
+                title="Blank Entry", message="Please fill in website name to search!"
+            )
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
@@ -25,23 +56,40 @@ def delete_info():
 
 
 def add_info():
+    new_data = {
+        website_entry.get().lower(): {
+            "email": email_entry.get(),
+            "password": password_entry.get(),
+            "note": note_entry.get(),
+        }
+    }
     if len(website_entry.get()) == 0 or len(password_entry.get()) == 0:
-        messagebox.showwarning(title="Oops!", message="Please fill in all the details!")
+        messagebox.showwarning(
+            title="Blank Value", message="Please fill in all the details!"
+        )
     else:
         is_ok = messagebox.askokcancel(
-            title=website_entry.get(),
+            title=website_entry.get().lower(),
             message=f"You've entered following details: \nEmail: {email_entry.get()} \nPassword:{password_entry.get()} \nIs it ok to save?",
         )
         if is_ok:
-            with open(FILE_PATH, "a") as f:
-                f.writelines(
-                    f"{website_entry.get()} | {email_entry.get()} | {password_entry.get()} | {note_entry.get()}\n"
-                )
+            # json.dump(new_data, f, indent=4)
+            try:
+                with open(FILE_PATH, "r") as f:
+                    data = json.load(f)
+            except FileNotFoundError:
+                with open(FILE_PATH, "w") as f:
+                    json.dump(new_data, f, indent=4)
+            else:
+                data.update(new_data)
+                with open(FILE_PATH, "w") as f:
+                    json.dump(data, f, indent=4)
+            finally:
                 delete_info()
-            messagebox.showinfo(
-                title="Success!",
-                message="Data successfully saved!",
-            )
+                messagebox.showinfo(
+                    title="Success!",
+                    message="Data successfully saved!",
+                )
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -55,9 +103,13 @@ canvas.create_image(100, 100, image=logo)
 canvas.grid(row=0, column=1)
 
 website_label = Label(text="Website", font=(FONT_NAME, 10)).grid(row=1, column=0)
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1, columnspan=1, sticky="EW")
 website_entry.focus()
+
+search_button = Button(text="Search", width=14, command=search).grid(
+    row=1, column=2, columnspan=1, sticky="EW"
+)
 
 email_label = Label(text="Email/Username", font=(FONT_NAME, 10)).grid(row=2, column=0)
 email_entry = Entry(width=35)
